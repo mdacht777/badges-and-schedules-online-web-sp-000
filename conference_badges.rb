82817e4fda7c35e4fd9a1f7b8c8780e5d2385965
Martin Achtermann <mdacht777@gmail.com>
2020-06-07 14:44:37 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  arr=[]
+  array.each do |name|
+  arr << "Hello, my name is #{name}."
+  end
+  arr
+end
+
+def assign_rooms(array)
+  arr=[]
+  i=1
+  array.each do |name|
+  arr << "Hello, #{name}! You'll be assigned to room #{i}!"
+  i+=1
+  end
+  arr
+end
+
+def printer(array)
+  arr1=batch_badge_creator(array)
+  arr2=assign_rooms(array)
+  i=0
+  arr1.each do |att|
+    puts att
+    puts arr2[i]
+    i+=1
+  end
+end
